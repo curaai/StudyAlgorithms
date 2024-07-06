9fb8b2d13192414de64e43cc1731bbcd85391a48
curaai <curaai00@gmail.com>
2024-07-06 03:55:34 +0000

    leetcode: 51. N-Queens

---
+++ python/leetcode/51.py
@@ -0,0 +1,32 @@
+from typing import List
+
+
+def solveNQueens(n: int) -> List[List[str]]:
+    res = [] 
+
+    def to_string(queens):
+        return list(map(lambda q: '.' * q[1] + 'Q' + '.' * (n-q[1]-1), queens))
+ 
+    def tracking(queens, cols):
+        if len(queens) == n:
+            return res.append(queens)
+
+        row = len(queens)
+        for col in range(n):
+            if col in cols:
+                continue 
+
+            if any(map(lambda q: abs(q[0] - row) == abs(q[1] - col), queens)):
+                continue
+
+            tracking(queens+[(row, col)], cols+[col])
+            
+
+    for i in range(int(n / 2) + 1):
+        queens = [(0, i)]
+
+        tracking(queens, [i])
+    
+    return list(map(to_string, res))
+    
+print(solveNQueens(4))
